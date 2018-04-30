4d1601fb5590ebb5e3ddc2005f751af1029a2418
Daniel Zawadzki <hello@danielzawadzki.com>
2018-04-30 16:20:26 +0200

    Removing song and playlist boilerplate models.

--- src/api/graphQLRouter.js
+++ src/api/graphQLRouter.js
@@ -1,7 +1,5 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import { userResolvers, userType } from './resources/user';
-import { songResolvers, songType } from './resources/song';
-import { playlistResolvers, playlistType } from './resources/playlist';
 import { ctSegmentResolvers, ctSegmentType } from './resources/ctSegment';
 import { partnerResolvers, partnerType } from './resources/partner';
 import {
@@ -22,8 +20,6 @@ const schema = makeExecutableSchema({
 	typeDefs: [
 		baseSchema,
 		userType,
-		songType,
-		playlistType,
 		partnerType,
 		ctSegmentType,
 		partnerSegmentType
@@ -31,8 +27,6 @@ const schema = makeExecutableSchema({
 	resolvers: merge(
 		{},
 		userResolvers,
-		songResolvers,
-		playlistResolvers,
 		partnerResolvers,
 		ctSegmentResolvers,
 		partnerSegmentResolvers
